f402eb3d79deb9edfc673d6419f434f1637981fc
magreb <arthurmendes1727@gmail.com>
2025-07-12 20:26:57 -0300

    feat: iniciando o projeto

---
+++ src/main/java/com/mycompany/atividadescomplementares/AtividadesComplementares.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadescomplementares;
+
+/**
+ *
+ * @author arthu
+ */
+public class AtividadesComplementares {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
